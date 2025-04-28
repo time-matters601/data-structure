87877db345afd7be95335b59b6958d0cd64a59a6
time-matters601 <sangashedrack601@gmail.com>
2025-04-28 13:26:38 +0300

    Add files via upload

---
+++ ArrayController.java
@@ -0,0 +1,30 @@
+package com.example.arraysdemo.contoller;
+
+import com.example.arraysdemo.service.ArrayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/arrays")
+public class ArrayController {
+
+    @Autowired
+    private ArrayService arrayService;
+
+    //1D Array endpoint
+    @PostMapping("/temperature")
+    public ResponseEntity<?> analyzeTemperatures(@RequestBody double[] temperatures) {
+        try {
+            return ResponseEntity.ok(arrayService.analyzeTemperatures(temperatures));
+        } catch (IllegalArgumentException e) {
+                        return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    //2D Array endpoint
+    @PostMapping("/transpose")
+    public int[][] transposematrix(@RequestBody int[][] matrix) { return arrayService.transposeMatrix(matrix);}
+}
